7c05358feec560c5db010c029b8ec2bd17e213c0
samrichards <spm.richards97@gmail.com>
2025-06-16 16:25:44 +0100

    Parsed csv data using panda. Added 2 app columns.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -12,3 +13,20 @@ with col2:
     content = """Hi, I'm Sam and I'm a mathematician, theoretical physicist, and programmer.
     """
     st.info(content)
+
+content2 = """Below you can find some of the apps I have built in python. Please feel free to contact me for more information!"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+data_frame = pd.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in data_frame[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in data_frame[10:].iterrows():
+        st.header(row["title"])
+
+
+
